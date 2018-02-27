ba2af8a52a04fc34ef458d713d220a454de57ccf
Sajal Agrawal <sajal.agrawal1997@gmail.com>
2018-02-28 01:00:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function enlarge(element){
+	element.style.transform="scale(1.1, 1.1)";
+	element.style.transition="0.6s ease";
+	//console.log(element);
+}
+function reduceBack(element){
+	element.style.transform="scale(1.0, 1.0)";
+}
